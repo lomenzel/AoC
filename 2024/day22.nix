be1a091e4c0af612e23a29b904d0b6d044ff23f5
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2024-12-23 14:47:04 +0000

    day23 part 1

---
+++ 2024/day22.nix
@@ -0,0 +1,40 @@
+with builtins; with (import ../lib.nix); let
+  realinput = readFile ./day22.input;
+
+  parseInput = input: input
+    |> lib.splitString "\n"
+    |> filter (e: e != "")
+    |> map lib.toInt;
+
+
+  next = secretNumber:
+    let
+      s64 = (secretNumber * 64);
+      s64' = bitXor secretNumber s64;
+      s64'' = modPositive s64' 16777216;
+
+      s32 = s64'' / 32;
+      s32' = bitXor s64'' s32;
+      s32'' = modPositive s32' 16777216;
+
+      s2048 = s32'' * 2048;
+       s2048' = bitXor s32'' s2048;
+      s2048'' = modPositive s2048' 16777216;
+    in 
+      s2048'';
+
+  nextN = n: secretNumber:
+    foldl' (acc: curr: next (trace (toString (price acc)) acc)) secretNumber (lib.range 1 n);
+
+  price = secretNumber: modPositive secretNumber 10;
+
+
+
+  part1 = input: input
+    |> parseInput
+    |> map (nextN 2000)
+    |> sum;
+
+in
+
+nextN 100 1
---
+++ 2024/day23.nix
@@ -0,0 +1,112 @@
+with builtins; with (import ../lib.nix); let
+  realinput = readFile ./day23.input;
+
+  testinput = ''
+    kh-tc
+    qp-kh
+    de-cg
+    ka-co
+    yn-aq
+    qp-ub
+    cg-tb
+    vc-aq
+    tb-ka
+    wh-tc
+    yn-cg
+    kh-ub
+    ta-co
+    de-co
+    tc-td
+    tb-wq
+    wh-td
+    ta-ka
+    td-qp
+    aq-cg
+    wq-ub
+    ub-vc
+    de-ta
+    wq-aq
+    wq-vc
+    wh-yn
+    ka-de
+    kh-ta
+    co-tc
+    wh-qp
+    tb-vc
+    td-yn
+  '';
+
+  parseInput = input: input
+    |> lib.splitString "\n"
+    |> filter (e: e!= "")
+    |> map (lib.splitString "-")
+    |> (e: 
+      {
+        connections = e ++ (map lib.reverseList e) 
+          |> lib.groupBy (head)
+          |> mapAttrs (name: value: map (e: elemAt e 1) value)
+          ;
+        computers = flat e |> lib.lists.unique;
+      }
+    );
+
+  thirdPCs = connections: l: 
+    lib.intersectLists connections.${head l} connections.${elemAt l 1} 
+      |> map (e: [(head l) (elemAt l 1) e]);
+
+  connectedToAll = connections: l:
+    if l == [] then abort "i expect something to connect to" else
+    foldl' (acc: curr: lib.intersectLists acc connections.${curr}) connections.${head l} (tail l)
+      |> lib.subtractLists l
+  ;
+
+  withNext = connections: l:
+    connectedToAll connections l
+      |> map (e: l ++ [e] |> lib.lists.naturalSort)
+      |> lib.lists.unique
+      #|> sort (a: b: a < b)
+      ;
+
+  part2 = input:
+    let i = parseInput input;
+    in with i;
+     map (e: [e]) computers
+     |> findLargestNet connections
+     |> head
+     |> join ","
+     ;
+
+  findLargestNet = connections: l:
+    let 
+      nextNet = map (withNext connections) (trace "length ${(toString (length l))} head ${toString (head l)}" l) |> flat;
+    in
+      if nextNet == [] then l else
+        nextNet
+        |> lib.unique
+        |> findLargestNet connections
+        |> lib.unique
+        ;
+  
+  part1 = input: 
+    let 
+      i = parseInput input;
+    in
+    with i;
+      (attrNames connections)
+        |> filter (e: head (lib.stringToCharacters e) == "t")
+        |> map (e: map (f: [e f]) connections.${e})
+        |> flat
+        |> map (thirdPCs connections)
+        |> flat
+        |> map lib.lists.naturalSort
+        |> lib.unique
+        |> length
+     ;
+
+
+  
+
+
+
+in
+  part2 realinput
